3ac06e5c743278b25f98b1427c2d741a1a9f5ebc
James Rea <jamesrea97@gmail.com>
2024-04-27 14:36:54 +0100

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A GoLang flake for sage.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        p = import nixpkgs {
+          inherit system;
+        };
+      in
+        {
+          devShells = rec {
+            default = nixpkgs.legacyPackages.${system}.mkShell {
+              packages = [
+                p.go
+                p.gopls
+                p.go-tools
+                p.errcheck
+                p.gofumpt
+              ];
+            };
+          };
+        }
+    );
+}
